db8cf37c9bc5b7cb6ea0cf4b4ad39a16ba625967
Katherine Gruenberg <katherine.gruenberg@gmail.com>
2018-04-07 07:36:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your solution here!
+const drivers = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendDriver(name) {
+  drivers.push("Ralph");
+};
+
+function destructivelyPrependDriver(name) {
+  drivers.unshift("Bob");
+};
+
+function destructivelyRemoveLastDriver() {
+  drivers.pop();
+};
